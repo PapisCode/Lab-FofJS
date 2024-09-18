c76fa6121b7e078957bbb5bfecf0aa8c4eb97112
PapisCode <gabhernandez@hotmail.ca>
2024-09-18 15:29:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+console.log("Hello World")
+
+var studentName = "Aubrey"; // declaring student's name
+
+console.log(studentName);
+
+var age = 30; // declaring student's age
+
+console.log(age);
+
+
+
+let StudentProgram = "CPAN"; // declaring the student's program
+const studentID = "N061068983"; // declaring the student's ID
+
+console.log(StudentProgram);
+console.log(studentID);
+
+let name = "Aubrey"
+var age = 30;
+
+let message = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message); // Output: Hello, my name is Aubrey and I am 30 years old.
+
+let found = true;
+let lost = false;
+console.log(found);
+console.log(lost);
+
+found = false;
+lost = true;
+
+
+// BigInt 
+let largeNumber = 67548890773425n; // a large integer beyond the safe limit for Number
+console.log(largeNumber);
+
+let firstName = "Aubrey";
+let middleName = 'Drake';
+let lastName = `Graham`;
+
+console.log("the name is Drake, arguably one of the greatest artist of all time.")
+
+let person = {
+    firstName: "Aubrey" ,
+    lastName: "Graham" ,
+    age: 30
+};
+
+console.log(person.firstName) ;
+
+let colors = ['Red', 'Green', 'Blue'];
+console.log(colors[0]); // Accesses the first element, output: 'Red'
+colors.push('Yellow'); // Adds a new element at the end
+console.log(colors); // Output: ['Red', 'Green', 'Blue', 'Yellow']
+colors.pop();
+console.log(colors);
+
+console.log(5 * 2); // Multiplication,
+console.log(10 / 2); // Division
+console.log(11 % 3); // Modulus (Remainder)
+
+console.log(5 + 3); // Addition
+console.log(10 - 2); // Subtraction
+
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
+
+
+
